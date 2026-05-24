f58a751ea656bb93300026c9f53e6013c78aba0e
BallBoii <thanakirt47@gmail.com>
2026-05-24 16:53:57 +0700

    Fix run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os, sys, runpy
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path[:0] = [ROOT, os.path.join(ROOT, "gui")]
-runpy.run_path(os.path.join(ROOT, "gui", "confocal_gui.py"), run_name="__main__")
+runpy.run_path(os.path.join(ROOT, "gui", "mainexp_gui.py"), run_name="__main__")
